933e17d1ca4707a1a6a9dc731af690cefca3cbcc
brendan <brendanrayw@gmail.com>
2021-07-20 15:24:15 -0700

    spawning new threads for each future to run on. added sleep rng to each task to confirm concurrency

--- my_tokio/src/main.rs
+++ my_tokio/src/main.rs
@@ -1,13 +1,41 @@
 mod my_tokio;
 
+use rand::{Rng, thread_rng};
+
 fn main() {
     let my_tokio = my_tokio::MyTokio::new();
 
     my_tokio.spawn(async {
+        let mut rng = thread_rng();
+        let mut rand: u64 = rng.gen();
+        rand = rand % 10;
+        println!("Sleep delay on task 1: {}", rand);
+        std::thread::sleep(std::time::Duration::new(rand % 10, 0));
         println!("Task 1");
     });
     my_tokio.spawn(async {
+        let mut rng = thread_rng();
+        let mut rand: u64 = rng.gen();
+        rand = rand % 10;
+        println!("Sleep delay on task 2: {}", rand);
+        std::thread::sleep(std::time::Duration::new(rand, 0));
         println!("Task 2");
+    });
+    my_tokio.spawn(async {
+        let mut rng = thread_rng();
+        let mut rand: u64 = rng.gen();
+        rand = rand % 10;
+        println!("Sleep delay on task 3: {}", rand);
+        std::thread::sleep(std::time::Duration::new(rand % 10, 0));
+        println!("Task 3");
+    });
+    my_tokio.spawn(async {
+        let mut rng = thread_rng();
+        let mut rand: u64 = rng.gen();
+        rand = rand % 10;
+        println!("Sleep delay on task 4: {}", rand);
+        std::thread::sleep(std::time::Duration::new(rand % 10, 0));
+        println!("Task 4");
     });
 
     my_tokio.run();
--- my_tokio/src/my_tokio.rs
+++ my_tokio/src/my_tokio.rs
@@ -1,43 +1,21 @@
 use std::{
-	sync::mpsc,
+	sync::{Arc, Mutex, mpsc},
 	pin::Pin,
 	task::{Context, Poll},
 	future::Future,
+	thread,
 };
-use futures::{
-	task,
-	executor,
-};
-use rand::Rng;
+use futures::task;
 
-type MyFuture = dyn Future<Output = ()> + 'static;
-
-struct MyFut {
-	future: Pin<Box<MyFuture>>,
-}
+type BoxedFuture = Mutex<Pin<Box<dyn Future<Output = ()> + Sync + Send>>>;
 
 pub struct MyTokio {
-	sender: mpsc::Sender<Pin<Box<MyFuture>>>,
-	receiver: mpsc::Receiver<Pin<Box<MyFuture>>>,
+	sender: mpsc::Sender<Arc<Task>>,
+	receiver: mpsc::Receiver<Arc<Task>>,
 }
 
-impl AsMut<MyFut> for MyFut {
-	fn as_mut(&mut self) -> &mut MyFut {
-		self
-	}
-}
-
-impl Future for MyFut {
-	type Output = ();
-
-	fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-		if false {
-			let mut new_self = *self;
-			new_self.future.as_mut().poll(cx);
-			return Poll::Ready(());
-		}
-		Poll::Pending
-	}
+pub struct Task {
+	future: BoxedFuture
 }
 
 impl MyTokio {
@@ -45,22 +23,23 @@ impl MyTokio {
 		let (tx, rx) = mpsc::channel();
 		MyTokio {sender: tx, receiver: rx}
 	}
-	pub fn spawn<T: Future<Output = ()> + 'static>(&self, fut: T) {
-		let boxed_fut = Box::pin(fut);
-		self.sender.send(boxed_fut).unwrap();
+	pub fn spawn<T:>(&self, fut: T)
+	where T: Future<Output = ()> + Sync + Send + 'static,
+	{
+		let task = Arc::new(Task {future: Mutex::new(Box::pin(fut))});
+		self.sender.send(task).unwrap();
 	}
-	pub fn run(&self) {
-		let waker = task::noop_waker();
-		let mut context = task::Context::from_waker(&waker);
+	pub fn run(self) {
 		loop {
-			let fut = self.receiver.recv().unwrap();
-			let mut new = Box::pin(fut);
-			let mut my_fut = Box::pin(MyFut {future: new});
-			let waker = task::noop_waker();
-			let mut cx = Context::from_waker(&waker);
-			if my_fut.as_mut().poll(&mut cx) == Poll::Pending {
-				println!("handle this");
-			}
+			let task = self.receiver.recv().unwrap();
+			thread::spawn(move ||{
+					let waker = task::noop_waker();
+					let mut cx = Context::from_waker(&waker);
+					let mut future = task.future.lock().unwrap();
+					if future.as_mut().poll(&mut cx) == Poll::Pending {
+						println!("handle this");
+					}
+			});
 		}
 	}
 }
