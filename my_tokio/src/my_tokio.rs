73a89709e0ab3758c46e81e36b726d7df3b97cc1
brendan <brendanrayw@gmail.com>
2021-07-20 20:09:54 -0700

    implemented arcwake for my Task and added a 5 second buffer to retry running the Task if it fails

--- my_tokio/src/my_tokio.rs
+++ my_tokio/src/my_tokio.rs
@@ -3,7 +3,6 @@ use std::{
 	pin::Pin,
 	task::{Context, Poll},
 	future::Future,
-	thread,
 };
 use threadpool;
 use futures::task;
@@ -11,17 +10,25 @@ use futures::task;
 type BoxedFuture = Mutex<Pin<Box<dyn Future<Output = ()> + Sync + Send>>>;
 
 pub struct MyTokio {
-	sender: mpsc::Sender<Arc<Task>>,
+	sender: mpsc::SyncSender<Arc<Task>>,
 	receiver: mpsc::Receiver<Arc<Task>>,
 }
 
 pub struct Task {
 	future: BoxedFuture,
+	sender: mpsc::SyncSender<Arc<Task>>,
+}
+
+impl task::ArcWake for Task {
+	fn wake_by_ref(arc_self: &Arc<Self>) {
+		let clone = arc_self.clone();
+		arc_self.sender.send(clone).unwrap();
+	}
 }
 
 impl MyTokio {
 	pub fn new() -> MyTokio {
-		let (tx, rx) = mpsc::channel();
+		let (tx, rx) = mpsc::sync_channel(10);
 		MyTokio {sender: tx, receiver: rx}
 	}
 	pub fn spawn<T:>(&self, fut: T)
@@ -29,21 +36,23 @@ impl MyTokio {
 	{
 		let task = Arc::new(Task {
 			future: Mutex::new(Box::pin(fut)),
+			sender: self.sender.clone(),
 		});
-		self.sender.send(task).unwrap();
+		self.sender.send(task).expect("Error");
 	}
 	pub fn run(self) {
 		let pool = threadpool::ThreadPool::new(5);
 		loop {
 			let task = self.receiver.recv().unwrap();
 			pool.execute(move ||{
-					let waker = task::noop_waker();
-					let mut cx = Context::from_waker(&waker);
-					let mut future = task.future.lock().unwrap();
-					if future.as_mut().poll(&mut cx) == Poll::Pending {
-						println!("handle this");
-					}
-					println!("executed on thread: {:?}", thread::current().id());
+				let mut future = task.future.lock().unwrap();
+				let waker = task::waker_ref(&task);
+				let mut cx = Context::from_waker(&waker);
+				if let Poll::Pending = future.as_mut().poll(&mut cx) {
+					println!("trying again in 5 seconds on thread {:?}", std::thread::current().id());
+					std::thread::sleep(std::time::Duration::new(5, 0));
+					cx.waker().wake_by_ref();
+				}
 			});
 		}
 	}
